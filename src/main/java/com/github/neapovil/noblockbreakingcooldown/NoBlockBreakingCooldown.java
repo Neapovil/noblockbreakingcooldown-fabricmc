1cb506b06186a26f7b11a5716e58a3965fcb6b66
William <nearata@protonmail.com>
2024-05-16 10:01:55 +0200

    init mod

---
+++ src/main/java/com/github/neapovil/noblockbreakingcooldown/NoBlockBreakingCooldown.java
@@ -0,0 +1,16 @@
+package com.github.neapovil.noblockbreakingcooldown;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public final class NoBlockBreakingCooldown implements ClientModInitializer
+{
+    public static final Logger LOGGER = LoggerFactory.getLogger("NoBlockBreakingCooldown");
+
+    @Override
+    public void onInitializeClient()
+    {
+    }
+}
